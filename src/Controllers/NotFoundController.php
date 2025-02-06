bb5fc37149dbaed4dc5837ab14dcb1ff5cf6df30
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-02-06 16:06:50 -0300

    Correção de variavel

--- src/Controllers/NotFoundController.php
+++ src/Controllers/NotFoundController.php
@@ -8,7 +8,7 @@ use App\Http\Response;
 class NotFoundController {
     
     public function index(Request $request, Response $response) {
-        $responsesssssssss::json([
+        $response::json([
             'error' => true,
             'success' => false,
             'message' => "Method not allowed",
