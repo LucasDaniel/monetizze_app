d87130d2856fc6d6f0db4e9602975a2a7847f21c
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-02-06 19:21:43 -0300

    Adicionando modulo service e validator

--- src/Controllers/TripulanteController.php
+++ src/Controllers/TripulanteController.php
@@ -4,12 +4,23 @@ namespace App\Controllers;
 
 use App\Http\Request;
 use App\Http\Response;
+use App\Services\TripulanteService;
 
 class TripulanteController {
     
     public function store(Request $request, Response $response) {
        
         $body = $request::body();
+
+        $tripulanteService = TripulanteService::create($body);
+
+        if (isset($tripulanteService['error'])) {
+            return $response::json([
+                'error' => true,
+                'success' => false,
+                'data' => $tripulanteService['error'],
+            ],400);
+        }
 
         $response::json([
             'error' => false,
