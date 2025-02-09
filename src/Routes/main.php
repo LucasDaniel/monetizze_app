1601e00c50e85d813935a828e253c03482cc4183
Lucas Daniel Beltrame <lucasdanielbeltrame@hotmail.com>
2025-02-09 18:36:48 -0300

    Deletando hello world

--- src/Routes/main.php
+++ src/Routes/main.php
@@ -5,7 +5,6 @@ use App\Http\Route;
 //Route::get('/migrate', 'HomeController@migrate');
 //Route::get('/seeder', 'HomeController@seeder');
 
-Route::get('/', 'HomeController@index');
 Route::post('/html/generate', 'HtmlController@generateHtml');
 Route::post('/tripulante/create', 'TripulanteController@store');
 Route::post('/tripulante-bilhete/create-random-numbers', 'TripulanteBilheteController@createRandomNumbers');
